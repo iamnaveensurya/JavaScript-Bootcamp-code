1b3f79b4b49e65cfc99c77441ec589a1ff9b6763
iamnaveensurya <naveensurya064@gmail.com>
2022-10-27 00:21:46 +0530

    conditional temperature message displayer

---
+++ Basics of Javascript code assignemnt/02Basics/04Conditionals.js
@@ -0,0 +1,13 @@
+// Need to display messages based on temperature
+
+var temperature = 49;
+
+if (temperature <= 20){
+    console.log("Its very cold outside");
+}
+else if(temperature <= 30){
+    console.log("Its modrate outside");
+}
+else{
+    console.log("Its very high temperature outside");
+}
