33ddc16cab15c478e04ba26982e06db0cc3f3f07
iamnaveensurya <naveensurya064@gmail.com>
2022-10-26 23:29:39 +0530

    Our first sign up form added

---
+++ Basics of Javascript code assignemnt/02Basics/01Variables.js
@@ -0,0 +1,21 @@
+var fullName = "Naveen Kumar";
+
+//How to name variables in Javascript
+
+// fullName full-name full_name
+
+var course_name = "Javascript development BootCamp";
+
+var isLoggedIn = false;
+
+var logged_Count = 03;
+
+console.log(logged_Count);
+
+var pay_ment_mode;
+
+console.log(pay_ment_mode);
+
+pay_ment_mode = "Credit Card";
+
+console.log(pay_ment_mode);
---
+++ Basics of Javascript code assignemnt/02Basics/02Variables-two.js
@@ -0,0 +1,30 @@
+const uid = "user9966";
+
+var userName = "iamnaveensurya";
+var firstName = "Naveen Kuamr";
+var lastName = "Ballanki";
+var email = "naveensurya064@gmail.com";
+var password = "123456";
+var confirmPassword = "123456";
+var countryFrom = "INDIA";
+var stateFrom = "Andhar Predesh";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+var isLoggedInFromFacebook = false;
+
+
+
+console.log(`
+    With Unique ID: ${uid}
+    User Name: ${userName}
+    User's first name is : ${firstName}
+    User's last name is : ${lastName}
+    and his email is: ${email}
+    and uses the password: ${password}
+    user is from country: ${countryFrom}
+    user is from state: ${stateFrom}
+    user's course Count is: ${courseCount}
+    user is logged in from Google: ${isLoggedInFromGoogle}
+    user is logged in from Facebook: ${isLoggedInFromFacebook}
+
+`);
